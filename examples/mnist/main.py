7bf3c274b5de31bd6cb0fb04905ec38997a29397
Olivier Breuleux <breuleux@gmail.com>
2021-09-23 11:48:12 -0400

    Restore MNIST to original

--- examples/mnist/main.py
+++ examples/mnist/main.py
@@ -10,7 +10,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-from giving import give, given
 from torchvision import datasets, transforms
 from torch.optim.lr_scheduler import StepLR
 
@@ -50,7 +49,12 @@ def train(args, model, device, train_loader, optimizer, epoch):
         loss = F.nll_loss(output, target)
         loss.backward()
         optimizer.step()
-        give(batch_idx, train_loss=loss)
+        if batch_idx % args.log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item()))
+            if args.dry_run:
+                break
 
 
 def test(model, device, test_loader):
@@ -58,17 +62,18 @@ def test(model, device, test_loader):
     test_loss = 0
     correct = 0
     with torch.no_grad():
-        for batch_idx, (data, target) in enumerate(test_loader):
+        for data, target in test_loader:
             data, target = data.to(device), target.to(device)
             output = model(data)
             test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
             pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
             correct += pred.eq(target.view_as(pred)).sum().item()
-            give(batch_idx)
 
     test_loss /= len(test_loader.dataset)
-    correct /= len(test_loader.dataset)
-    give(test_loss, correct)
+
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
 
 
 def run(args):
@@ -99,217 +104,16 @@ def run(args):
     test_loader = torch.utils.data.DataLoader(dataset2, **test_kwargs)
 
     model = Net().to(device)
-    give()
     optimizer = optim.Adadelta(model.parameters(), lr=args.lr)
 
     scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
     for epoch in range(1, args.epochs + 1):
-        with give.inherit(epoch=epoch):
-            with give.wrap_inherit("train", mode="train", batch_size=train_loader.batch_size, length=len(train_loader.dataset)):
-                train(args, model, device, train_loader, optimizer, epoch)
-
-            with give.wrap_inherit("test", mode="test", batch_size=test_loader.batch_size, length=len(test_loader.dataset)):
-                test(model, device, test_loader)
-
+        train(args, model, device, train_loader, optimizer, epoch)
+        test(model, device, test_loader)
         scheduler.step()
 
     if args.save_model:
         torch.save(model.state_dict(), "mnist_cnn.pt")
-
-
-def log_all(gv):
-    """Log everything that is given. This is useful for debugging."""
-    gv.display()
-
-
-def log_simple(gv):
-    """Log a few select fields."""
-
-    # List of fields we will display
-    to_display = [
-        "mode",
-        "batch_idx",
-        "train_loss",
-        "test_loss",
-        "correct",
-    ]
-
-    # The | operator merges two streams, here we have
-    # * The stream of train_loss entries, throttled to one every 0.5 seconds
-    # * The stream of test_loss entries, not throttled
-    data = gv.where("train_loss").throttle(0.5) | gv.where("test_loss")
-
-    # Only keep the keys we want to display, and then display them
-    data.keep(*to_display).display()
-
-
-def log_terminal(gv):
-    """Custom logging on the terminal."""
-
-    # Install wrappers around the train and test wrap_inherits
-    @gv.kwrap("train")
-    @gv.kwrap("test")
-    def _(mode):
-        # Printed before we enter the block
-        print(f"Start {mode}")
-        yield
-        # Printed after we exit the block
-        print(f"End {mode}")
-
-    # Take entries that contain train_loss, then every 0.5 seconds, do:
-    @gv.where("train_loss").throttle(0.5).ksubscribe
-    def _(batch_idx, batch_size, length, epoch, train_loss):
-        # Every named argument corresponds to an entry in the dictionary
-        # produced by give(). batch_idx and train_loss are given together
-        # at the same time, whereas batch_size, length and epoch are also
-        # given because of wrap_inherit
-        n = (batch_idx + 1) * batch_size
-        progress = f"{n}/{length} {n / length:.0%}"
-        print(
-            f"  Train Epoch #{epoch} [{progress}] Loss: {train_loss:.6f}"
-        )
-
-    # Now we will do the same thing for test_loss, but differently.
-    # Use whichever technique fits your brain better :)
-
-    def _progress(batch_idx, batch_size, length):
-        n = (batch_idx + 1) * batch_size
-        return f"{n}/{length} {n / length:.0%}"
-
-    # * test_loss is only provided at the end of the test
-    # * augment() adds a key computed from the others
-    # * print() takes a format string (but no f prefix!)
-    gv \
-        .where("batch_idx", mode="test") \
-        .throttle(0.5) \
-        .augment(progress=_progress) \
-        .print("  Test Epoch #{epoch} [{progress}]")
-
-    # These are given at the end of the test block
-    @gv.where("test_loss", "correct").ksubscribe
-    def _(test_loss, correct):
-        print(f"  Test loss: {test_loss:.6f}")
-        print(f"  Accuracy:  {correct:.0%}")
-
-
-def log_rich(gv):
-    """Create a simple terminal dashboard using rich.
-
-    This displays a live table of the last value for everything given,
-    with a progress bar for the current task under it.
-    """
-
-    from rich.progress import Progress
-    from rich.live import Live
-    from rich.table import Table
-    from rich.pretty import Pretty
-    from rich.console import Group
-
-    # Current rows are stored here
-    rows = {}
-
-    # First, a table with the latest value of everything that was given
-    table = Table.grid(padding=(0, 3, 0, 0))
-    table.add_column("key", style="bold green")
-    table.add_column("value")
-
-    # Below, a progress bar for the current task (train or test)
-    progress = Progress(auto_refresh=False)
-    current_task = progress.add_task("----")
-
-    # Group them
-    grp = Group(table, progress)
-
-    # This will wrap Live around the run block (the whole main function)
-    gv.wrap("run", Live(grp, refresh_per_second=4))
-
-    # This refreshes the progress bar when we change task
-    @gv.kwrap("train")
-    @gv.kwrap("test")
-    def _(epoch, length, mode):
-        # Note: the difference between wrap and kwrap is that kwrap
-        # takes a function that passes the data as keyword arguments,
-        # whereas wrap passes no arguments.
-        descr = f"Epoch #{epoch}" if mode == "train" else "Test"
-        progress.reset(current_task, total=length, description=descr)
-        yield
-
-    # This sets the progress bar's completion meter
-    @gv.where("batch_idx").ksubscribe
-    def _(batch_idx, batch_size):
-        progress.update(current_task, completed=batch_idx * batch_size)
-
-    # This updates the table every time we get new values
-    @gv.subscribe
-    def _(values):
-        for k, v in values.items():
-            if isinstance(v, torch.Tensor):
-                # Some special processing for torch.Tensor, we will
-                # only display shape and type if it's not a scalar
-                if v.shape:
-                    v = (v.shape, v.dtype)
-                else:
-                    v = v.item()
-            if k in rows:
-                rows[k]._object = v
-            else:
-                rows[k] = Pretty(v)
-                table.add_row(k, rows[k])
-
-
-def log_wandb(gv, args):
-    """Log data into Weights and Biases."""
-
-    # Only import if needed :)
-    import wandb
-
-    # Initialize the project
-    entity, project = args.wandb.split(":")
-    wandb.init(project=project, entity=entity, config=vars(args))
-
-    # Watch the model's weights. We only do it the first time we see the model.
-    gv["?model"].first() >> wandb.watch
-
-    # Log train_loss, test_loss and correct (see the plots in the wandb dash)
-    gv.keep("train_loss", "test_loss", "correct") >> wandb.log
-
-
-def log_mlflow(gv, args):
-    """Log data into MLFlow."""
-
-    import mlflow
-
-    mlflow.log_params(vars(args))
-
-    gv.keep("train_loss", "test_loss", "correct") >> mlflow.log_metrics
-
-
-def log_comet(gv, args):
-    """Log data into CometML."""
-
-    import comet_ml
-
-    entity, project, api_key = args.comet.split(":")
-
-    # Create an experiment with your api key
-    experiment = comet_ml.Experiment(
-        api_key=api_key,
-        project_name=project,
-        workspace=entity,
-        auto_output_logging=False,
-    )
-
-    experiment.log_parameters(vars(args))
-
-    # CometML uses context managers, which we can conveniently plug in here
-    gv.wrap("train", experiment.train)
-    gv.wrap("test", experiment.test)
-
-    # Set epoch and step whenever we give them
-    gv["?epoch"] >> experiment.set_epoch
-    gv["?batch_idx"] >> experiment.set_step
-
-    gv.keep("train_loss", "test_loss", "correct") >> experiment.log_metrics
 
 
 def main():
@@ -335,44 +139,8 @@ def main():
                         help='how many batches to wait before logging training status')
     parser.add_argument('--save-model', action='store_true', default=False,
                         help='For Saving the current Model')
-    parser.add_argument('--wandb', default="",
-                        help='username:project for wandb')
-    parser.add_argument('--comet', default="",
-                        help='username:project for comet')
-    parser.add_argument('--mlflow', action='store_true', default=False,
-                        help='whether to use mlflow to store logs')
-    parser.add_argument('--rich', action='store_true', default=False,
-                        help='whether to show a rich display')
-    parser.add_argument('--simple', action='store_true', default=False,
-                        help='whether to use the simple display method')
-    parser.add_argument('--display-all', action='store_true', default=False,
-                        help='whether to display all given data')
     args = parser.parse_args()
-
-    with given() as gv:
-
-        if args.display_all:
-            log_all(gv)
-        elif args.rich:
-            log_rich(gv)
-        elif args.simple:
-            log_simple(gv)
-        else:
-            log_terminal(gv)
-
-        if args.wandb:
-            log_wandb(gv, args)
-
-        if args.comet:
-            log_comet(gv, args)
-
-        if args.mlflow:
-            log_mlflow(gv, args)
-
-        give(args=vars(args))
-
-        with give.wrap("run"):
-            run(args)
+    run(args)
 
 
 if __name__ == '__main__':
